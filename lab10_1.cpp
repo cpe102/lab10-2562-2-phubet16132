6e8352082b174d1b1cb76bc71f99c22d5d5656a2
phubet16132 <59276795+phubet16132@users.noreply.github.com>
2020-01-31 15:26:48 +0700

    Update lab10_1.cpp

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -1,2 +1,42 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include<iostream>
+#include<string>
+#include<cstdlib>
+#include<ctime>
+
+using namespace std;
+
+
+int main (){
+	
+	string ent1="",ent2="",ent3="";
+	
+	srand(time(0));
+	
+	cout << "Press Enter 3 times to reveal your future.\n";
+	
+	int i=rand()%9;
+	
+	getline(cin,ent1);
+	getline(cin,ent2);
+	getline(cin,ent3);
+	
+	string g[9]={"A","B+","B","C+","C","D+","D","F","W"};
+	
+  	if(ent1=="" and ent2=="" and ent3==""){
+  		
+  		cout << "You will get " << g[i] << " in this 261102.";
+  		
+	  }
+	
+	
+	
+	
+	
+	return 0;
+}
+
+
+
+
+
+
